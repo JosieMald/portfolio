a06501ed0f7f9dd29c2730a9735fc2f45c461f5a
Lucre <josielindamaldonado@gmail.com>
2022-08-22 20:32:02 -0400

    added hamburger menu to mobile view.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('world')
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    mobileMenu.classList.remove("active");
+}))
