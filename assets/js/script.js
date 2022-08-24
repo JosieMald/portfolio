41de8c58f79bc0aa4249c7d8b33bed0cd2b27c6e
Lucre <josielindamaldonado@gmail.com>
2022-08-24 08:43:18 -0400

    formatted all documents.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     mobileMenu.classList.remove("active");
-}))
+  })
+);
